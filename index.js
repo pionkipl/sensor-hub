0fd6fca5e8e77497b6f58f858b78533c68a4d9ca
pionkipl <jarek491@gmail.com>
2019-10-15 22:32:23 +0200

    Add Body Parser.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+console.log('Siema');
