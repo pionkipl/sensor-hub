9d21fd9ae230fd4e2bc5d01412f1e9cb68566cf5
pionkipl <jarek491@gmail.com>
2019-10-17 22:27:08 +0200

    Add measurement route.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const measurements = require('./measurements');
 
 const app = express();
 const port = 3000;
@@ -7,10 +8,9 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.json({"message": "Welcome to EasyNotes application. Take notes quickly. Organize and keep track of all your notes."});
-});
+app.use('/measurements', measurements);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.listen(port);
 
 console.log('Siema');
