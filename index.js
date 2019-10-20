35b20e0a6f54e851cf7d2e48d4fb969bc662f043
Bartosz Walicki <bartosz.walicki@appverk.com>
2019-10-21 00:00:22 +0200

    PostgreSQL is now server from docker. Test connection with Sequalize performed.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const measurements = require('./measurements');
+const express = require("express");
+const bodyParser = require("body-parser");
+const measurements = require("./measurements");
+const database = require("./database/connection");
 
 const app = express();
 const port = 3000;
@@ -8,9 +9,8 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/measurements', measurements);
-
+app.use("/measurements", measurements);
 
 app.listen(port);
 
-console.log('Siema');
+database.initConnection();
